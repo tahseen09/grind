ebec4198b0e603002c6da3932014f2c53c8f286c
Tahseen Rahman <tahseen.rahman0996@gmail.com>
2022-05-19 20:51:18 +0530

    Factorial using Recursion

---
+++ factorial.py
@@ -0,0 +1,8 @@
+# ==============================================================================
+# * Factorial using Recursion
+# ! Time complexity - O(n!)
+# ==============================================================================
+def factorial(n: int) -> int:
+    return 1 if n == 1 else n * factorial(n-1)
+
+print(factorial(100))
